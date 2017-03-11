170590b36694c4641d2561512cb8efe66bae3680
Ubuntu <ubuntu@ubuntu-xenial.localdomain>
2017-03-11 06:36:38 +0000

    First version

---
+++ app/components/App.js
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var App = React.createClass({
+  render: function() {
+    return (
+      <div className="shopping-list">
+        <h1>Shopping List</h1>
+      </div>
+    );
+  }
+})
+
+module.exports = App;
